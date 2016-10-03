90f31c160cb5f23ed4f714475956c81a9575aa8e
Josh McBroom <mcbroomjc@gmail.com>
2016-10-03 16:24:15 -0700

    Added zipcodes package module

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// Requiring our Node Package Modules
+var isValidZip = require('is-valid-zip');
+var zipcodes = require('zipcodes');
+
+// Initializing Angular module
 var weatherApp = angular.module('weatherApp', []);
 
-// weatherApp.controller('MainCtrl', )
+// Building our Angular Controller
+weatherApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.zipCodeInput = '';
+
+  $scope.getForecast() {
+    var req = {
+      url: 'https://api.darksky.net/forecast/' + MY_SECRET_KEY + '/' + lat + ',' + long,
+      method: 'GET',
+    }
+  }
+}]);
